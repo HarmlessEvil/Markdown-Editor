ec03ab9efe8f6a4234e01f11b78bdbaaf482f59b
Alexander Chori <alexandrchori@gmail.com>
2021-04-02 01:20:41 +0300

    Add autocomplete

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,10 +1,13 @@
 <template>
-  <codemirror v-model="code" :options="options" id="editor" :spell-check="spellCheck" />
+  <codemirror v-model="code" :options="options" id="editor" :spell-check="spellCheck" @changes="showSuggestion"/>
 </template>
 
 <script>
+import debounce from 'debounce'
+
 import {codemirror} from 'vue-codemirror/src/index'
 import 'codemirror/addon/mode/overlay'
+import 'codemirror/addon/hint/show-hint'
 import 'codemirror/mode/markdown/markdown'
 
 import ruDic from 'dictionary-ru/index.dic'
@@ -12,7 +15,10 @@ import ruAff from 'dictionary-ru/index.aff'
 import enDic from 'dictionary-en/index.dic'
 import enAff from 'dictionary-en/index.aff'
 
+import autocomplete from '@/assets/data/autocomplete.json'
+
 import 'codemirror/lib/codemirror.css'
+import 'codemirror/addon/hint/show-hint.css'
 
 export default {
   name: "Editor",
@@ -41,6 +47,36 @@ export default {
         }
       }
     }
+  },
+  methods: {
+    showSuggestion: debounce(function (codemirror) {
+      codemirror.showHint({
+        completeSingle: false,
+        hint(editor) {
+          const cursor = editor.getCursor()
+
+          const words = editor.getLine(cursor.line)
+              .split(/[!"#$%&()*+,-./:;<=>?@[\\\]^_`{|}~ ]/)
+              .filter(it => it !== "")
+          const word = words.length === 0 ? null : words[words.length - 1]
+
+          if (word === null) {
+            return null
+          }
+
+          const suggestions = autocomplete[word]
+          if (suggestions === undefined) {
+            return null
+          }
+
+          return {
+            list: suggestions,
+            from: { line: cursor.line, ch: cursor.ch - word.length },
+            to: cursor
+          }
+        }
+      })
+    }, 150)
   }
 }
 </script>
